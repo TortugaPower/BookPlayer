56c47ca3cfaba25d096a7eef475a585810a8090f
Gianni Carlo <gcarlo89@hotmail.com>
2018-09-20 14:52:39 -0500

    Add empty watch app, enable capability App Groups

---
+++ BookPlayerWatch Extension/InterfaceController.swift
@@ -0,0 +1,30 @@
+//
+//  InterfaceController.swift
+//  BookPlayerWatch Extension
+//
+//  Created by Gianni Carlo on 9/20/18.
+//  Copyright © 2018 Tortuga Power. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
