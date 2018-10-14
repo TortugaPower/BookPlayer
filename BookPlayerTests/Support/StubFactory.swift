7c054d839382e8ea95d75671b7d90247328109a6
ryantstone <ryan@ryanstone.io>
2018-10-14 14:39:52 -0400

    Update tests to use stub factory

--- BookPlayerTests/Support/StubFactory.swift
+++ BookPlayerTests/Support/StubFactory.swift
@@ -2,12 +2,12 @@ import Foundation
 @testable import BookPlayer
 
 class StubFactory {
-    public static func book(title: String, duration: Double) -> Book {
+    public static func book(title: String, duration: Double, metaDataTitle: String? = nil) -> Book {
         let dummyUrl    = URL(fileURLWithPath: title)
         let bookUrl     = FileItem(originalUrl: dummyUrl, processedUrl: dummyUrl, destinationFolder: dummyUrl)
         let book        = DataManager.createBook(from: bookUrl)
         book.duration   = duration
-        
+
         return book
     }
 }
