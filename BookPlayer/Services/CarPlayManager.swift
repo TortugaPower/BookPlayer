02c2c92c0a927692d44a66ca6711a8d05d0f291c
Florian Pichler <fp@ylk.gd>
2018-10-17 00:08:42 +0200

    Improved CarPlay support
    - progress display
    - use a unique item identifier to display album artwork at any level
    - additional cell in playlists to continue with first unfinished item

--- BookPlayer/Services/CarPlayManager.swift
+++ BookPlayer/Services/CarPlayManager.swift
@@ -13,6 +13,8 @@ enum BookPlayerError: Error {
     case UnableToLoadBooks(String)
 }
 
+let playlistItemsOffset: Int = 1
+
 class CarPlayManager: NSObject, MPPlayableContentDataSource, MPPlayableContentDelegate {
     static let shared = CarPlayManager()
 
@@ -26,6 +28,8 @@ class CarPlayManager: NSObject, MPPlayableContentDataSource, MPPlayableContentDe
         MPPlayableContentManager.shared().reloadData()
     }
 
+    // MARK: - MPPlayableContentDataSource
+
     func numberOfChildItems(at indexPath: IndexPath) -> Int {
         if indexPath.indices.count == 0 {
             return self.library.items?.count ?? 0
@@ -35,31 +39,52 @@ class CarPlayManager: NSObject, MPPlayableContentDataSource, MPPlayableContentDe
             return 0
         }
 
-        return count
+        return count + playlistItemsOffset
     }
 
     func beginLoadingChildItems(at indexPath: IndexPath, completionHandler: @escaping (Error?) -> Void) {
         completionHandler(nil)
     }
 
+    func childItemsDisplayPlaybackProgress(at indexPath: IndexPath) -> Bool {
+        if indexPath.count == 2 && indexPath[1] == 0 {
+            return false
+        }
+
+        return true
+    }
+
     func contentItem(at indexPath: IndexPath) -> MPContentItem? {
-        guard let libraryItem = self.library.getItem(at: indexPath) else {
+        if indexPath.count == 2 && indexPath[1] == 0 {
+            let contentItem = MPContentItem(identifier: UUID().uuidString)
+
+            contentItem.title = "Continue with first unfinished file"
+            contentItem.isPlayable = false
+
+            return contentItem
+        }
+
+        guard let libraryItem = self.getLibraryItem(at: indexPath) else {
             return nil
         }
 
-        let contentItem = MPContentItem.init()
+        let contentItem = MPContentItem(identifier: libraryItem.identifier)
 
         contentItem.title = libraryItem.title
-        contentItem.playbackProgress = Float(libraryItem.currentTime / libraryItem.duration)
+        contentItem.playbackProgress = libraryItem.isCompleted ? 1.0 : Float(libraryItem.currentTime / libraryItem.duration)
 
         if let book = libraryItem as? Book {
-            contentItem.subtitle = "\(libraryItem.isCompleted ? "✓ " : "")\(book.author ?? "")"
+            contentItem.subtitle = book.author ?? ""
             contentItem.isPlayable = true
         }
 
-        if libraryItem is Playlist {
+        if let playlist = libraryItem as? Playlist {
             contentItem.isContainer = true
-            contentItem.subtitle = "\(libraryItem.isCompleted ? "✓" : "")"
+            contentItem.subtitle = "No files"
+
+            if let count = playlist.books?.count, count > 0 {
+                contentItem.subtitle = "\(count) file\(count != 1 ? "s" : "")"
+            }
         }
 
         contentItem.artwork = MPMediaItemArtwork(boundsSize: libraryItem.artwork.size, requestHandler: { (_) -> UIImage in
@@ -69,18 +94,10 @@ class CarPlayManager: NSObject, MPPlayableContentDataSource, MPPlayableContentDe
         return contentItem
     }
 
+    // MARK: - MPPlayableContentDelegate
+
     func playableContentManager(_ contentManager: MPPlayableContentManager, initiatePlaybackOfContentItemAt indexPath: IndexPath, completionHandler: @escaping (Error?) -> Void) {
-        if indexPath.indices.count == 0 {
-            return
-        }
-
-        var item = self.library.getItem(at: indexPath[0])
-
-        if let playlist = item as? Playlist, indexPath.indices.count == 2 {
-            item = playlist.getBook(at: indexPath[1])
-        }
-
-        guard let book = item as? Book else {
+        guard indexPath.indices.count > 0, let book = self.getBook(at: indexPath) else {
             return
         }
 
@@ -97,7 +114,47 @@ class CarPlayManager: NSObject, MPPlayableContentDataSource, MPPlayableContentDe
         }
     }
 
+    func getBook(at indexPath: IndexPath) -> Book? {
+        let item = self.library.getItem(at: indexPath[0])
+
+        if let playlist = item as? Playlist, indexPath.indices.count == 2 {
+            if indexPath[0] == 0 {
+                return playlist.getBookToPlay()
+            }
+
+            return playlist.getBook(at: indexPath[1] - playlistItemsOffset)
+        }
+
+        if let book = item as? Book {
+            return book
+        }
+
+        return nil
+    }
+
     func playableContentManager(_ contentManager: MPPlayableContentManager, didUpdate context: MPPlayableContentManagerContext) {
         print("Update")
+    }
+
+    // MARK: - Helpers
+
+    func getLibraryItem(at indexPath: IndexPath) -> LibraryItem? {
+        if indexPath.indices.count == 0 || indexPath.count == 2 && indexPath[1] == 0 {
+            return nil
+        }
+
+        guard let item = self.library.getItem(at: indexPath[0]) else {
+            return nil
+        }
+
+        guard
+            indexPath.indices.count == 2,
+            let playlist = item as? Playlist,
+            let book = playlist.getBook(at: indexPath[1] - playlistItemsOffset)
+            else {
+                return item
+        }
+
+        return book
     }
 }
