ce89a0e4de7a9b946763fcc0092dee367fd82ddd
Gianni Carlo <gcarlo89@hotmail.com>
2019-04-25 20:57:48 -0500

    Add watch icon assets

---
+++ BookPlayerWatch Extension/Notification+BookPlayerWatchApp.swift
@@ -0,0 +1,11 @@
+//
+//  Notification+BookPlayerWatch.swift
+//  BookPlayerWatch Extension
+//
+//  Created by Gianni Carlo on 5/4/19.
+//  Copyright © 2019 Tortuga Power. All rights reserved.
+//
+
+import Foundation
+
+    public static let lastBook = Notification.Name("com.tortugapower.audiobookplayer.watch.lastBook")
