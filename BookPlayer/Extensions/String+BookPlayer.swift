a62a2f66def92a8691f4502446cf12d0d81cd1ca
Gianni Carlo <gcarlo89@hotmail.com>
2020-02-11 08:30:16 -0500

    Remove localization from storyboards

---
+++ BookPlayer/Extensions/String+BookPlayer.swift
@@ -0,0 +1,15 @@
+//
+//  String+BookPlayer.swift
+//  BookPlayer
+//
+//  Created by Gianni Carlo on 2/11/20.
+//  Copyright © 2020 Tortuga Power. All rights reserved.
+//
+
+import UIKit
+
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+}
