78d5a6f236ae22fcc938e91ed98525ea1afa6297
Gianni Carlo <gcarlo89@hotmail.com>
2021-01-26 08:13:55 -0500

    Remove unused var

--- Shared/Extensions/URL+BookPlayer.swift
+++ Shared/Extensions/URL+BookPlayer.swift
@@ -1,21 +1,5 @@
 import Foundation
 public extension URL {
-    /// Isolates and returns the parent directory of a `URL`
-    var parentDirectory: URL {
-        get {
-            let parent = self.deletingLastPathComponent()
-            return parent
-        }
-        set {
-            let newParentDirectory = newValue
-            let fileNameFromBefore = self.lastPathComponent
-            let fileNameInNewDirectory = newParentDirectory
-                .appendingPathComponent(fileNameFromBefore)
-            // Replace the URL with the new one:
-            self = fileNameInNewDirectory
-        }
-    }
-    
     /// Isolates and returns a filename string from a `URL`
     var fileName: String {
         get {
