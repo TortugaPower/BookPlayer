044e19bf142a2d7650b31063b3c256c666a8f8a4
Gianni Carlo <gcarlo89@hotmail.com>
2021-11-18 21:04:21 -0500

    Add icloud backups setting

--- BookPlayer/Settings/SettingsViewModel.swift
+++ BookPlayer/Settings/SettingsViewModel.swift
@@ -6,6 +6,18 @@
 //  Copyright © 2021 Tortuga Power. All rights reserved.
 //
 
+import BookPlayerKit
 import Foundation
 
-class SettingsViewModel: BaseViewModel<SettingsCoordinator> { }
+class SettingsViewModel: BaseViewModel<SettingsCoordinator> {
+  func toggleFileBackupsPreference(_ flag: Bool) {
+    UserDefaults.standard.set(flag, forKey: Constants.UserDefaults.iCloudBackupsEnabled.rawValue)
+
+    // Modify the processed folder to be considered for backups
+    var resourceValues = URLResourceValues()
+    resourceValues.isExcludedFromBackup = !flag
+    var processedFolderURL = DataManager.getProcessedFolderURL()
+
+    try? processedFolderURL.setResourceValues(resourceValues)
+  }
+}
