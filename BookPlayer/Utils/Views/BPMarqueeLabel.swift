fa3465e9905f7cb525e0717f8d262d44a3a768cf
Gianni Carlo <gcarlo89@hotmail.com>
2021-12-05 21:54:27 -0500

    Adapt player to support left-to-right languages

--- BookPlayer/Utils/Views/BPMarqueeLabel.swift
+++ BookPlayer/Utils/Views/BPMarqueeLabel.swift
@@ -16,7 +16,5 @@ class BPMarqueeLabel: MarqueeLabel {
         self.animationDelay = 2.0
         self.speed = .rate(7.5)
         self.fadeLength = 10.0
-        self.leadingBuffer = 10.0
-        self.trailingBuffer = 10.0
     }
 }
