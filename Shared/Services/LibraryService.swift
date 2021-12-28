2c7be710608cb6e5e887b349bfc746fde1f0bb36
Gianni Carlo <gcarlo89@hotmail.com>
2021-12-27 21:28:19 -0500

    Fix folder contents order when importing

--- Shared/Services/LibraryService.swift
+++ Shared/Services/LibraryService.swift
@@ -582,7 +582,12 @@ public final class LibraryService: LibraryServiceProtocol {
       files.append(fileURL)
     }
 
-    _ = self.insertItems(from: files, into: folder, library: library, processedItems: [])
+    let sortDescriptor = NSSortDescriptor(key: "path", ascending: true, selector: #selector(NSString.localizedStandardCompare(_:)))
+    let orderedSet = NSOrderedSet(array: files)
+    // swiftlint:disable force_cast
+    let sortedFiles = orderedSet.sortedArray(using: [sortDescriptor]) as! [URL]
+
+    _ = self.insertItems(from: sortedFiles, into: folder, library: library, processedItems: [])
   }
 
   public func moveItems(_ items: [LibraryItem], inside relativePath: String?, moveFiles: Bool) throws {
