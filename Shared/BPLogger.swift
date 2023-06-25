8a3c7a4611bf0ebfa162308b024a4d017826c8d4
Gianni Carlo <gcarlo89@hotmail.com>
2023-06-25 11:43:43 -0500

    Add debug file functionality to BPLogger

--- Shared/BPLogger.swift
+++ Shared/BPLogger.swift
@@ -18,4 +18,41 @@ extension BPLogger {
       category: String(describing: Self.self)
     )
   }
+
+  public static func logFile(message: String) {
+    let debugLogFileURL = DataManager.getProcessedFolderURL().appendingPathComponent("debug_log_file.txt")
+
+    do {
+      try message.appendLineToURL(fileURL: debugLogFileURL)
+    }
+    catch {
+      logger.debug("Could not write to log file")
+    }
+  }
+}
+
+private extension Data {
+  func append(fileURL: URL) throws {
+    if let fileHandle = FileHandle(forWritingAtPath: fileURL.path) {
+      defer {
+        fileHandle.closeFile()
+      }
+      fileHandle.seekToEndOfFile()
+      fileHandle.write(self)
+    }
+    else {
+      try write(to: fileURL, options: .atomic)
+    }
+  }
+}
+
+private extension String {
+  func appendLineToURL(fileURL: URL) throws {
+    try (">>> " + self + "\n").appendToURL(fileURL: fileURL)
+  }
+
+  func appendToURL(fileURL: URL) throws {
+    let data = self.data(using: String.Encoding.utf8)!
+    try data.append(fileURL: fileURL)
+  }
 }
