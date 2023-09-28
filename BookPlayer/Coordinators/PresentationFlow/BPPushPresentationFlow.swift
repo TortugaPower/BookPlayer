4b5127569add5f6571c15a6a8d0f81c4c83f1b8f
Duong Thai <dvthai.dev@gmail.com>
2023-09-28 14:00:15 +0700

    remove "unowned" references because "structs" don't retain reference cycles

--- BookPlayer/Coordinators/PresentationFlow/BPPushPresentationFlow.swift
+++ BookPlayer/Coordinators/PresentationFlow/BPPushPresentationFlow.swift
@@ -9,15 +9,15 @@
 import UIKit
 
 /// Handle the push presentation requirements for Coordinators
-public struct BPPushPresentationFlow: BPCoordinatorPresentationFlow {
+struct BPPushPresentationFlow: BPCoordinatorPresentationFlow {
   /// Navigation used for the flow of the coordinator
-  public unowned let navigationController: UINavigationController
+  let navigationController: UINavigationController
   /// First controller on the ``navigationController`` stack before starting the presentation
-  unowned let initiatingController: UIViewController?
+  let initiatingController: UIViewController?
 
   /// Initializer
   /// - Parameter navigationController: Controller used for the flow of the coordinator
-  public init(navigationController: UINavigationController) {
+  init(navigationController: UINavigationController) {
     self.navigationController = navigationController
     self.initiatingController = navigationController.viewControllers.last
   }
@@ -26,13 +26,13 @@ public struct BPPushPresentationFlow: BPCoordinatorPresentationFlow {
   /// - Parameters:
   ///   - viewController: The starting `UIViewController` in the coordinator's flow
   ///   - animated: Specifies if we want the push transition animated or not
-  public func startPresentation(_ viewController: UIViewController, animated: Bool) {
+  func startPresentation(_ viewController: UIViewController, animated: Bool) {
     navigationController.pushViewController(viewController, animated: animated)
   }
 
   /// Pops to the initiating UIViewController or to the root UIViewController if there wasn't an initiating controller
   /// - Parameter animated: Specifies if we want the dismiss transition animated or not
-  public func finishPresentation(animated: Bool) {
+  func finishPresentation(animated: Bool) {
     if let initiatingController = initiatingController {
       navigationController.popToViewController(initiatingController, animated: animated)
     } else {
@@ -45,7 +45,7 @@ public struct BPPushPresentationFlow: BPCoordinatorPresentationFlow {
 extension BPCoordinatorPresentationFlow {
   /// Push presentation for the coordinator flow
   /// - Parameter navigationController: Controller used for the flow of the coordinator
-  public static func pushFlow(
+  static func pushFlow(
     navigationController: UINavigationController
   ) -> Self where Self == BPPushPresentationFlow {
     return .init(navigationController: navigationController)
