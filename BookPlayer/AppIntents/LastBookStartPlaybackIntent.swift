2571a5ff86286944032a52e057a8301207659fe7
Gianni Carlo <gcarlo89@hotmail.com>
2023-09-30 13:31:17 -0500

    Create new app intents and shortcuts

---
+++ BookPlayer/AppIntents/LastBookStartPlaybackIntent.swift
@@ -0,0 +1,46 @@
+//
+//  LastBookStartPlaybackIntent.swift
+//  BookPlayer
+//
+//  Created by Gianni Carlo on 30/9/23.
+//  Copyright © 2023 Tortuga Power. All rights reserved.
+//
+
+import Foundation
+import AppIntents
+
+@available(iOS 16.0, macOS 14.0, watchOS 10.0, *)
+struct LastBookStartPlaybackIntent: AudioStartingIntent {
+  static var title: LocalizedStringResource = "Cancel Sleep Timer"
+
+  func perform() async throws -> some IntentResult {
+    let stack = try await DatabaseInitializer().loadCoreDataStack()
+    
+    guard let appDelegate = await AppDelegate.shared else {
+      throw "AppDelegate is not available"
+    }
+
+    let coreServices = await appDelegate.createCoreServicesIfNeeded(from: stack)
+
+    guard let book = coreServices.libraryService.getLastPlayedItems(limit: 1)?.first else {
+      throw "There's no last played book"
+    }
+
+    await appDelegate.loadPlayer(
+      book.relativePath,
+      autoplay: true,
+      showPlayer: nil,
+      alertPresenter: VoidAlertPresenter()
+    )
+
+    return .result()
+  }
+}
+
+struct VoidAlertPresenter: AlertPresenter {
+  func showAlert(_ title: String?, message: String?, completion: (() -> Void)?) {}
+  
+  func showLoader() {}
+  
+  func stopLoader() {}
+}
