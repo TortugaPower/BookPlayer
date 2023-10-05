d2994aaccab92cd6d7fcde6cbba0d7248d18c7a6
Duong Thai <dvthai.dev@gmail.com>
2023-10-05 15:09:31 +0700

    - `ItemDetailsView` only does:     1. pass `ItemDetailsFormViewModel` down to `ItemDetailsForm`          - they have identical preview code     1. a small compile-time condition for its `body` - my changes:     1. move `ItemDetailsForm.body` up to `ItemDetailsView`     2. remove `ItemDetailsForm`

--- BookPlayer/Library/ItemDetails Screen/Views/ItemDetailsView.swift
+++ BookPlayer/Library/ItemDetails Screen/Views/ItemDetailsView.swift
@@ -8,16 +8,79 @@
 
 import BookPlayerKit
 import SwiftUI
+import PhotosUI
 
 struct ItemDetailsView: View {
+  /// View model for the form
   @ObservedObject var viewModel: ItemDetailsFormViewModel
+  /// Theme view model to update colors
+  @StateObject var themeViewModel = ThemeViewModel()
+  /// Flag to show action sheet for the artwork
+  @State private var showingArtworkOptions = false
+  /// Flag to show the ImagePicker
+  @State private var showingImagePicker = false
+  /// Flag to show the empty pasteboard alert
+  @State private var showingEmptyPasteboardAlert = false
 
   var body: some View {
+    let form = Form {
+      Section(header: Text("section_item_title".localized)
+        .foregroundColor(themeViewModel.secondaryColor)
+      ) {
+        ClearableTextField(viewModel.titlePlaceholder, text: $viewModel.title)
+      }
+      .listRowBackground(themeViewModel.secondarySystemBackgroundColor)
+
+      if viewModel.showAuthor {
+        Section(header: Text("section_item_author".localized)
+          .foregroundColor(themeViewModel.secondaryColor)
+        ) {
+          ClearableTextField(viewModel.authorPlaceholder, text: $viewModel.author)
+        }
+        .listRowBackground(themeViewModel.secondarySystemBackgroundColor)
+      }
+
+      ItemDetailsArtworkSectionView(image: $viewModel.selectedImage) {
+        showingArtworkOptions = true
+      }
+      .listRowBackground(themeViewModel.secondarySystemBackgroundColor)
+      .actionSheet(isPresented: $showingArtworkOptions) {
+        ActionSheet(
+          title: Text("artwork_options_title".localized),
+          buttons: [
+            .default(Text("artwork_photolibrary_title".localized)) {
+              showingImagePicker = true
+            },
+            .default(Text("artwork_clipboard_title".localized)) {
+              if let image = UIPasteboard.general.image {
+                viewModel.selectedImage = image
+              } else {
+                showingEmptyPasteboardAlert = true
+              }
+            },
+            .cancel(),
+          ]
+        )
+      }
+    }
+    .onChange(of: viewModel.selectedImage, perform: { _ in
+      viewModel.artworkIsUpdated = true
+    })
+    .sheet(isPresented: $showingImagePicker) {
+      ImagePicker(image: $viewModel.selectedImage)
+    }
+    .alert(isPresented: $showingEmptyPasteboardAlert) {
+      Alert(
+        title: Text("artwork_clipboard_empty_title".localized),
+        dismissButton: .default(Text("ok_button".localized))
+      )
+    }
+    .environmentObject(themeViewModel)
+
     if #available(iOS 16.0, *) {
-      ItemDetailsForm(viewModel: viewModel)
-        .scrollContentBackground(.hidden)
+      return form.scrollContentBackground(.hidden)
     } else {
-      ItemDetailsForm(viewModel: viewModel)
+      return form
     }
   }
 }
@@ -45,5 +108,6 @@ struct ItemDetailsView_Previews: PreviewProvider {
         )
       )
     )
+    .previewLayout(.sizeThatFits)
   }
 }
