f76b80d5e7b7cc0b992a772d744d46b522d38958
Gianni Carlo <gcarlo89@hotmail.com>
2023-12-30 23:47:49 -0500

    Remove completion when processing artwork for folders

--- Shared/Artwork/AVAudioAssetImageDataProvider.swift
+++ Shared/Artwork/AVAudioAssetImageDataProvider.swift
@@ -10,6 +10,8 @@ import AVFoundation
 import Foundation
 import Kingfisher
 
+/// Note: This provider does not take into account items at the DB level, only at the disk level
+/// so for folders where all the items are offloaded, it won't find items to parse an artwork
 public struct AVAudioAssetImageDataProvider: ImageDataProvider {
 
   public enum ProviderError: Error {
