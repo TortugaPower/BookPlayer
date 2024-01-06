4e86b3200b98b20e1b61fe3ab413efe04a54feea
Gianni Carlo <gcarlo89@hotmail.com>
2024-01-05 22:53:16 -0500

    Add new activity item source to share on the Mac

--- BookPlayer/Player/PlayerManager.swift
+++ BookPlayer/Player/PlayerManager.swift
@@ -603,7 +603,7 @@ final class PlayerManager: NSObject, PlayerManagerProtocol {
 
 extension PlayerManager {
   func jumpToChapter(_ chapter: PlayableChapter) {
-    jumpTo(chapter.start + 0.5, recordBookmark: false)
+    jumpTo(chapter.start + 0.1, recordBookmark: false)
   }
 
   func initializeChapterTime(_ time: Double) {
