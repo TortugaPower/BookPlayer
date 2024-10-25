aaed8af9c590647c51e1fbaaa999d75206499266
Lysann Tranvouez <lysann.tranvouez@gmail.com>
2024-10-25 23:03:36 +0200

    prefer early outs

--- BookPlayer/Jellyfin/Connection Screen/JellyfinConnectionViewController.swift
+++ BookPlayer/Jellyfin/Connection Screen/JellyfinConnectionViewController.swift
@@ -83,35 +83,40 @@ class JellyfinConnectionViewController: UIViewController, MVVMControllerProtocol
 
   private func bindConnectionObservers() {
     Publishers.CombineLatest(viewModel.$connectionState, $apiTask).sink { [weak self] (state, task) in
+      guard let self = self else {
+        return
+      }
       if let _ = task {
-        self?.navBarRightButtonEnabledWatcher = nil
-        self?.navigationItem.rightBarButtonItem?.isEnabled = false
-      } else if let self = self {
-        switch state {
-        case .disconnected:
-          self.navigationItem.rightBarButtonItem = UIBarButtonItem(
-            title: "jellyfin_connect_button".localized,
-            style: .done,
-            target: self,
-            action: #selector(self.didTapConnect)
-          )
-          navBarRightButtonEnabledWatcher = viewModel.form.$serverUrl.map { !$0.isEmpty }.sink { [weak self] canConnect in
-            self?.navigationItem.rightBarButtonItem?.isEnabled = canConnect
-          }
-        case .foundServer:
-          self.navigationItem.rightBarButtonItem = UIBarButtonItem(
-            title: "jellyfin_login_button".localized,
-            style: .done,
-            target: self,
-            action: #selector(self.didTapLogin)
-          )
-          navBarRightButtonEnabledWatcher = Publishers.CombineLatest3(viewModel.form.$username, viewModel.form.$password, $apiClient).sink { [weak self] (username, password, apiClient) in
-            self?.navigationItem.rightBarButtonItem?.isEnabled = apiClient != nil && !username.isEmpty && !password.isEmpty
-          }
-        case .connected:
-          navBarRightButtonEnabledWatcher = nil
-          self.navigationItem.rightBarButtonItem?.isEnabled = false
+        self.navBarRightButtonEnabledWatcher?.cancel()
+        self.navBarRightButtonEnabledWatcher = nil
+        self.navigationItem.rightBarButtonItem?.isEnabled = false
+        return
+      }
+      switch state {
+      case .disconnected:
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(
+          title: "jellyfin_connect_button".localized,
+          style: .done,
+          target: self,
+          action: #selector(self.didTapConnect)
+        )
+        navBarRightButtonEnabledWatcher = viewModel.form.$serverUrl.map { !$0.isEmpty }.sink { [weak self] canConnect in
+          self?.navigationItem.rightBarButtonItem?.isEnabled = canConnect
         }
+      case .foundServer:
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(
+          title: "jellyfin_login_button".localized,
+          style: .done,
+          target: self,
+          action: #selector(self.didTapLogin)
+        )
+        navBarRightButtonEnabledWatcher = Publishers.CombineLatest3(viewModel.form.$username, viewModel.form.$password, $apiClient).sink { [weak self] (username, password, apiClient) in
+          self?.navigationItem.rightBarButtonItem?.isEnabled = apiClient != nil && !username.isEmpty && !password.isEmpty
+        }
+      case .connected:
+        self.navBarRightButtonEnabledWatcher?.cancel()
+        self.navBarRightButtonEnabledWatcher = nil
+        self.navigationItem.rightBarButtonItem?.isEnabled = false
       }
     }
     .store(in: &disposeBag)
@@ -122,7 +127,7 @@ class JellyfinConnectionViewController: UIViewController, MVVMControllerProtocol
   }
 
   @objc func didTapConnect() {
-    if let _ = apiTask {
+    if apiTask != nil {
       return
     }
 
@@ -130,41 +135,42 @@ class JellyfinConnectionViewController: UIViewController, MVVMControllerProtocol
     let clientName = mainBundleInfo?[kCFBundleNameKey as String] as? String
     let clientVersion = mainBundleInfo?[kCFBundleVersionKey as String] as? String
     let deviceID = UIDevice.current.identifierForVendor
-    if let url = URL(string: viewModel.form.serverUrl), let clientName, let clientVersion, let deviceID {
-      let configuration = JellyfinClient.Configuration(
-        url: url,
-        client: clientName,
-        deviceName: UIDevice.current.name,
-        deviceID: "\(deviceID.uuidString)-\(clientName)",
-        version: clientVersion
-      )
-      let apiClient = JellyfinClient(configuration: configuration)
+    guard let url = URL(string: viewModel.form.serverUrl), let clientName, let clientVersion, let deviceID else {
+      return
+    }
+    let configuration = JellyfinClient.Configuration(
+      url: url,
+      client: clientName,
+      deviceName: UIDevice.current.name,
+      deviceID: "\(deviceID.uuidString)-\(clientName)",
+      version: clientVersion
+    )
+    let apiClient = JellyfinClient(configuration: configuration)
 
-      apiTask = Task {
-        defer { self.apiTask = nil }
-        let publicSystemInfo = try await apiClient.send(Paths.getPublicSystemInfo)
-        self.viewModel.connectionState = .foundServer
-        self.viewModel.form.serverName = publicSystemInfo.value.serverName
-        self.apiClient = apiClient
-      }
+    apiTask = Task {
+      defer { self.apiTask = nil }
+      let publicSystemInfo = try await apiClient.send(Paths.getPublicSystemInfo)
+      self.viewModel.connectionState = .foundServer
+      self.viewModel.form.serverName = publicSystemInfo.value.serverName
+      self.apiClient = apiClient
     }
   }
 
   @objc func didTapLogin() {
-    if let _ = apiTask {
+    if apiTask != nil {
       return
     }
-    if let apiClient {
-      let username = viewModel.form.username
-      let password = viewModel.form.password
-      apiTask = Task {
-        defer { self.apiTask = nil }
-        let authResult = try await apiClient.signIn(username: username, password: password)
-        if let _ = authResult.accessToken {
-          self.viewModel.connectionState = .connected
-          self.viewModel.handleConnectedEvent(forClient: apiClient)
-        }
-        self.apiTask = nil
+    guard let apiClient else {
+      return
+    }
+    let username = viewModel.form.username
+    let password = viewModel.form.password
+    apiTask = Task {
+      defer { self.apiTask = nil }
+      let authResult = try await apiClient.signIn(username: username, password: password)
+      if let _ = authResult.accessToken {
+        self.viewModel.connectionState = .connected
+        self.viewModel.handleConnectedEvent(forClient: apiClient)
       }
     }
   }
