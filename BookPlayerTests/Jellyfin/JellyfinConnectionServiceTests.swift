b8dd30bf52af95652ad89b5b8cd7e65b3cd21d09
Lysann Tranvouez <lysann.tranvouez@gmail.com>
2024-11-22 17:49:57 +0100

    first jellyfin tests + keychain service mock

--- BookPlayerTests/Jellyfin/JellyfinConnectionServiceTests.swift
+++ BookPlayerTests/Jellyfin/JellyfinConnectionServiceTests.swift
@@ -9,15 +9,112 @@
 @testable import BookPlayer
 import BookPlayerKit
 import XCTest
+import Combine
 
 class JellyfinConnectionServiceTests: XCTestCase {
+  var mockKeychain: KeychainServiceMock!
   var sut: JellyfinConnectionService!
   
   override func setUp() {
-    sut = JellyfinConnectionService(keychainService: KeychainService())
+    mockKeychain = KeychainServiceMock()
+    sut = JellyfinConnectionService(keychainService: mockKeychain)
   }
   
-  func testCreateService() {
-    XCTAssert(sut != nil)
+  func makeMockConnectionData(serverUrlString: String = "http://example.com") -> JellyfinConnectionData {
+    return JellyfinConnectionData(url: URL(string: serverUrlString)!,
+                                  serverName: "Mock Server",
+                                  userID: "42",
+                                  userName: "Mock User",
+                                  accessToken: "12345")
+  }
+  
+  func testNoConnectionByDefault() throws {
+    XCTAssertNil(try mockKeychain.get<JellyfinConnectionData>(.jellyfinConnection))
+    XCTAssertNil(sut.connection)
+  }
+  
+  func testNewInstanceLoadsSavedConnection() throws {
+    do {
+      let connectionData = makeMockConnectionData()
+      try mockKeychain.set(connectionData, key: .jellyfinConnection)
+    }
+    
+    let newInstance = JellyfinConnectionService(keychainService: mockKeychain)
+    
+    XCTAssertNotNil(newInstance.connection)
+    XCTAssertEqual(newInstance.connection?.url, URL(string: "http://example.com")!)
+    XCTAssertEqual(newInstance.connection?.serverName, "Mock Server")
+    XCTAssertEqual(newInstance.connection?.userID, "42")
+    XCTAssertEqual(newInstance.connection?.userName, "Mock User")
+    XCTAssertEqual(newInstance.connection?.accessToken, "12345")
+  }
+  
+  func testSetAndGetConnection() {
+    XCTAssertNil(sut.connection)
+    
+    sut.setConnection(makeMockConnectionData(), saveToKeychain: false)
+    
+    XCTAssertNotNil(sut.connection)
+    XCTAssertEqual(sut.connection?.url, URL(string: "http://example.com")!)
+    
+    sut.setConnection(makeMockConnectionData(serverUrlString: "http://example.com:8096"), saveToKeychain: false)
+    
+    XCTAssertNotNil(sut.connection)
+    XCTAssertEqual(sut.connection?.url, URL(string: "http://example.com:8096")!)
+  }
+  
+  func testSetConnectionPublishesChanges() throws {
+    var eventsPublished = 0
+    var latestConnectionDataInEvent: JellyfinConnectionData?
+    
+    var disposeBag = Set<AnyCancellable>()
+    sut.$connection.dropFirst().sink { newConnection in
+      eventsPublished += 1
+      latestConnectionDataInEvent = newConnection
+    }
+    .store(in: &disposeBag)
+    
+    sut.setConnection(makeMockConnectionData(), saveToKeychain: false)
+    
+    XCTAssertEqual(eventsPublished, 1)
+    XCTAssertNotNil(latestConnectionDataInEvent)
+    XCTAssertEqual(latestConnectionDataInEvent?.url, URL(string: "http://example.com")!)
+    
+    sut.setConnection(makeMockConnectionData(serverUrlString: "http://example.com:8096"), saveToKeychain: false)
+    
+    XCTAssertEqual(eventsPublished, 2)
+    XCTAssertNotNil(latestConnectionDataInEvent)
+    XCTAssertEqual(latestConnectionDataInEvent?.url, URL(string: "http://example.com:8096")!)
+  }
+  
+  func testDontSaveToKeychain() throws {
+    do {
+      let dataInKeychain: JellyfinConnectionData? = try mockKeychain.get(.jellyfinConnection)
+      XCTAssertNil(dataInKeychain)
+    }
+    XCTAssertNil(sut.connection)
+    sut.setConnection(makeMockConnectionData(), saveToKeychain: false)
+    
+    do {
+      let dataInKeychain: JellyfinConnectionData? = try mockKeychain.get(.jellyfinConnection)
+      XCTAssertNil(dataInKeychain)
+    }
+    
+    let newInstance = JellyfinConnectionService(keychainService: mockKeychain)
+    XCTAssertNil(newInstance.connection)
+  }
+  
+  func testSaveToKeychainAndGetWithNewInstance() throws {
+    XCTAssertNil(sut.connection)
+    sut.setConnection(makeMockConnectionData(), saveToKeychain: true)
+    
+    do {
+      let dataInKeychain: JellyfinConnectionData? = try mockKeychain.get(.jellyfinConnection)
+      XCTAssertNotNil(dataInKeychain)
+    }
+    
+    let newInstance = JellyfinConnectionService(keychainService: mockKeychain)
+    XCTAssertNotNil(newInstance.connection)
+    XCTAssertEqual(newInstance.connection?.url, URL(string: "http://example.com")!)
   }
 }
